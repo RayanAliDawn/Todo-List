4a935758e7934a4d480552f40da101474ff05e67
RayanAliDawn <102025889+RayanAliDawn@users.noreply.github.com>
2023-03-31 17:21:17 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       return item;
     });
     setItems(updatedItems);
-    setCount(count - 1);
+
   };
 
   const handleFilterChange = newFilter => {
@@ -45,11 +45,11 @@ function App() {
 
   return (
     <div className='Container w-full '>
-      <div className='flex flex-col justify-center items-center m-4 bg-white '>
-        <h1 className='m-8 font-semibold text-gray-600 text-[2rem] '> THINGS TO DO</h1>
+      <div className='flex flex-col justify-center items-center m-4 bg-white p-1 shadow-2xl'>
+        <h1 className='m-8 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-gray-500 from-10% via-sky-500 via-30% to-gray-500 to-90% text-[1.9rem] '> THINGS TO DO</h1>
 
-        <form onSubmit={handleNewItemSubmit} className=" w-full ">
-          <input className="w-full p-2 bg-white" placeholder='Add New '
+        <form onSubmit={handleNewItemSubmit} className=" w-full shadow ">
+          <input className="w-full p-2 drop-shadow-xl bg-white " placeholder='Add New Here '
             type="text"
             value={newItemTitle}
             onChange={event => setNewItemTitle(event.target.value)}
@@ -57,7 +57,7 @@ function App() {
           />
 
         </form>
-        <ul className='w-full text-gray-600  '>
+        <ul className='w-full text-gray-600 py-2 '>
 
           {filteredItems.map(item => (
             <li key={item.id} className="text-[1.5rem]">
@@ -75,7 +75,7 @@ function App() {
             </li>
           ))}
         </ul>
-        <div className='  w-full flex justify-around text-[0.9rem]   h-10 p-2  bg-lime-50'>
+        <div className='  w-full flex justify-around text-[0.9rem]   h-10 p-2 bg-gradient-to-r from-gray-300 from-10% via-sky-200 via-30% to-gray-300 to-90%'>
           <h1>{count}  left</h1>
           <div >
             <button className='cursor-pointer   ' onClick={() => handleFilterChange("completed")}>Completed</button>
@@ -84,12 +84,17 @@ function App() {
           </div>
 
 
+
         </div>
 
+        <span className="relative flex  h-9 w-36 justify-center items-center">
+
+          <span className="animate-ping absolute inline-flex h-7 w-30 opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-gray-900 from-10% via-sky-900 via-30% to-gray-900 to-90% text-[0.9rem]">Keep going!</span>
+
+        </span>
 
 
       </div>
-
     </div>
 
   )
